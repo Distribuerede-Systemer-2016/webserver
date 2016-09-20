7ee6f7947f39a3ea82726bc82893b98ee622a35b
Jesper Bruun Hansen <jesper@mentalact.dk>
2016-09-20 09:16:31 +0200

    implement runnable

---
+++ src/ResponseHandler.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ * Created by jesperbruun on 20/09/2016.
+ */
+public class ResponseHandler implements Runnable {
+
+  private Socket remoteSocket;
+
+  public ResponseHandler(Socket remoteSocket) {
+    this.remoteSocket = remoteSocket;
+  }
+
+  @Override
+  public void run() {
+
+    try {
+      System.out.println("Connection!");
+
+      //Read from input stream (from client)
+      BufferedReader inFromClient = new BufferedReader(new InputStreamReader(this.remoteSocket.getInputStream()));
+
+      //Print request headers from client
+      String str = ".";
+      while (!str.equals("")) {
+        str = inFromClient.readLine();
+        //System.out.println(str);
+      }
+
+
+      //Create output stream (to client)
+      DataOutputStream outToClient = new DataOutputStream(this.remoteSocket.getOutputStream());
+
+
+      //Write response headers
+      outToClient.writeBytes("HTTP/1.0 200 OK\n");
+      outToClient.writeBytes("Content-Type: application/json\n");
+      outToClient.writeBytes("Server: Hackerbot\n");
+      outToClient.writeBytes("\n");
+
+      Thread.sleep(1000);
+
+      outToClient.writeBytes("{\"hello\":\"world\"}");
+
+      //Flush'n'close
+      outToClient.flush();
+
+      outToClient.close();
+      this.remoteSocket.close();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+}
--- src/WebServer.java
+++ src/WebServer.java
@@ -7,7 +7,6 @@ import java.net.Socket;
 
 public class WebServer {
 
-
   private int port;
   private ServerSocket serverSocket;
 
@@ -31,44 +30,12 @@ public class WebServer {
         //Hang while-loop and wait for socket request
         Socket remoteSocket = this.serverSocket.accept();
 
-        System.out.println("Connection!");
-
-        //Read from input stream (from client)
-        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(remoteSocket.getInputStream()));
-
-        //Print request headers from client
-        String str = ".";
-        while (!str.equals("")) {
-          str = inFromClient.readLine();
-          //System.out.println(str);
-        }
-
-
-        //Create output stream (to client)
-        DataOutputStream outToClient = new DataOutputStream(remoteSocket.getOutputStream());
-
-
-        //Write response headers
-        outToClient.writeBytes("HTTP/1.0 200 OK\n");
-        outToClient.writeBytes("Content-Type: application/json\n");
-        outToClient.writeBytes("Server: Hackerbot\n");
-        outToClient.writeBytes("\n");
-
-        Thread.sleep(1000);
-
-        outToClient.writeBytes("{\"hello\":\"world\"}");
-
-        //Flush'n'close
-        outToClient.flush();
-
-        outToClient.close();
-        remoteSocket.close();
+        Runnable responseHandler = new ResponseHandler(remoteSocket);
+        new Thread(responseHandler).start();
 
       }
 
     } catch (IOException e) {
-      e.printStackTrace();
-    } catch (InterruptedException e) {
       e.printStackTrace();
     }
   }
